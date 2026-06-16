a03e15b312b5bc0fb3f47bec109b35cf9b35cc47
angelofonseca <angeloniemeyer97@gmail.com>
2026-06-16 10:47:45 -0300

    atividades 6.8 e 6.9

--- main.py
+++ main.py
@@ -1,4 +1,40 @@
+from tabulate import tabulate
+
 ocorrencias = []
+atendimentos = []
+historico_acoes = []
+historico_ocorrencias = []
+historico_atendimentos = []
+
+def gerencia_historico_acoes(acao):
+    while True:
+        print("\n===== MENU =====")
+        print("1 - Listar ocorrências")
+        print("2 - Listar atendimentos")
+        print("3 - Desfazer última ocorrência")
+        print("4 - Desfazer último atendimento")
+        print("0 - Sair")
+
+        opcao = input("Escolha uma opção: ")
+
+        if opcao == "1":
+            listar_ocorrencias()
+        elif opcao == "2":
+            listar_atendimentos()
+        elif opcao == "3":
+            desfazer_ultima_acao(historico_ocorrencias)
+        elif opcao == "4":
+            desfazer_ultima_acao(historico_atendimentos)    
+        elif opcao == "0":
+            print("Saindo...")
+            break
+        else:
+            print("Opção inválida.")
+
+def desfazer_ultima_acao(pilha):
+    resultado = pilha.pop()
+    print(f"Ocorrência apagada: {resultado}")
+
 
 def gerar_id(nome):
     soma = 0
@@ -30,7 +66,7 @@ def cadastrar_ocorrencia():
     print("Prioridade:", prioridade)
 
     ocorrencias.append({
-        'id_ocorrencia': id_ocorrencia,
+        'id': id_ocorrencia,
         'nome': nome,
         'tipo': tipo,
         'descricao': descricao,
@@ -40,13 +76,15 @@ def cadastrar_ocorrencia():
     print("Ocorrência salva com sucesso.")
 
 
-def listar_ocorrencias():
-        
+def listar_atendimentos():
+    print("\nLISTAR ATENDIMENTOS")
+    print("Aqui serão listados os atendimentos.")
+    print(tabulate(atendimentos, headers="keys", tablefmt="grid"))
+
+def listar_ocorrencias():  
     print("\nLISTAR OCORRÊNCIAS")
     print("Aqui serão listadas as ocorrências cadastradas.")
-    for x in ocorrencias:
-        print(x)
-
+    print(tabulate(ocorrencias, headers="keys", tablefmt="grid"))
 
 def buscar_ocorrencia():
     print("\nBUSCAR OCORRÊNCIA")
